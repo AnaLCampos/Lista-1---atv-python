7b2c02ec099df793fe8c2d6c1e360238209fac28
AnaLCampos <125393053+AnaLCampos@users.noreply.github.com>
2023-04-11 09:51:15 -0300

    Update atv14.py

--- L1 - PY/atv14.py
+++ L1 - PY/atv14.py
@@ -1,6 +1,6 @@
-valorproduto = float(input("Digite o valor do produto: "))
+Valorproduto = float(input("Digite o valor do produto: "))
 
-desconto = valorproduto * 0.09
-novo_valor = valorproduto - desconto
+Desconto = valorproduto * 0.09
+novovalor = valorproduto - desconto
 
-print("Valor com desconto de 9%: R${:.2f}".format(novo_valor))
+print("Valor com desconto de 9%: R${:.2f}".format(novovalor))

3fa64ab5e0bacf62827a36f74b8389df81fe86c6
AnaLCampos <125393053+AnaLCampos@users.noreply.github.com>
2023-04-11 10:37:59 -0300

    Update atv8.py

--- L1 - PY/atv8.py
+++ L1 - PY/atv8.py
@@ -2,10 +2,10 @@ import math
 
 pi = math.pi
 
-raio = float(input("Digite o raio do círculo: "))
+Raio = float(input("Digite o raio do círculo: "))
 
-circunferencia = 2 * pi * raio
-area = pi * raio**2
+Circunferencia = 2 * pi * Raio
+Area = pi * Raio**2
 
-print("Circunferência:", round(circunferencia, 2))
-print("Área:", round(area, 2))
+print("Circunferência:", round(Circunferencia, 2))
+print("Área:", round(Area, 2))

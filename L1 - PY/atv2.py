fc83d2f426f94ca8f8cc570f003ebdd1573ea410
AnaLCampos <125393053+AnaLCampos@users.noreply.github.com>
2023-04-11 10:32:38 -0300

    Update atv2.py

--- L1 - PY/atv2.py
+++ L1 - PY/atv2.py
@@ -1,3 +1,3 @@
-caractere1 = input("Digite o primeiro caractere: ")
-caractere2 = input("Digite o segundo caractere: ")
-print(f"O usuário digitou {caractere1} e {caractere2}!")
+Caractere1 = input("Digite o primeiro caractere: ")
+Caractere2 = input("Digite o segundo caractere: ")
+print(f"O usuário digitou {Caractere1} e {Caractere2}!")
